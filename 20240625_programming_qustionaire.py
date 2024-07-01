f4f0978c2cc60c6130beaf556dd2fd06895a9df7
Supermath101 <>
2024-07-01 13:06:05 -0400

    20240627 hardware software - day 3

--- 20240625_programming_qustionaire.py
+++ 20240625_programming_qustionaire.py
@@ -2,9 +2,9 @@ def main():
     name = "Professor Nedd"
     print("We want to know if you like programming!")
     print()
-    print("Do you like programming" + name + "?")
+    print("Do you like programming, " + name + "?")
     answer = input()
-    print("Great! You said" + answer + "?")
+    print("Great! You said \"" + answer + "\".")
     print("Let's start learning some Python today")
 
 if __name__ == "__main__":
