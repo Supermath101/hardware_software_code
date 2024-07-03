8853172760b313eb4d5b16c8d2406989e90794c2
Supermath101 <>
2024-07-03 13:02:42 -0400

    Hardware and Software project 2

---
+++ 20240620_programming_questionnaire_v2.py
@@ -0,0 +1,9 @@
+def main():
+    print("We want to know if you like programming!")
+    name = input("What is your name? ")
+    answer = input("Do you like programming " + name + "? ")
+    print("Great! you said, \"" + answer + "!\"")
+    print("Let's learn some Python today!!!")
+
+if __name__ == "__main__":
+    main()
